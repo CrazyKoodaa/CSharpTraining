de6277c1c5114b77e53e3483fbfa83c993f9c7a6
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-25 14:32:29 +0100

    subclass can call class method with the "base" Keyword

---
+++ Chapter6/animalSimulator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace animalSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Vertebrate
+    {
+        public virtual void Eat(Food morsel)
+        {
+            Swallow(morsel);
+            Digest();
+        }
+    }
+
+    class Chameleon : Vertebrate
+    {
+        public override void Eat(Food morsel)
+        {
+            CatchWithTongue(morsel);
+            /* The next two lines can be replaced
+            Swallow(morsel);
+            Digest();
+            * with 
+            */
+            base.Eat(morsel);
+        }
+    }
+}
