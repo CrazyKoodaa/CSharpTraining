907e5b070cc0cb4e950d02b5cee49237cf93a495
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-26 14:12:33 +0100

    Playing around with Interfaces

---
+++ Chapter7/PlayingAroundWithInterfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PlayingAroundWithInterfaces
+{
+    class Tallguy : IClown
+    { 
+        public string Name;
+        public int Height;
+
+        public string FunnyThingIHave => "ballon";
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk");
+        }
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My Name is {Name} and I'm {Height} inches tall");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tallguy tallguy = new Tallguy() {  Height = 76, Name = "Jimmy"};
+            tallguy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallguy.FunnyThingIHave}");
+            tallguy.Honk();
+
+        }
+    }
+}
