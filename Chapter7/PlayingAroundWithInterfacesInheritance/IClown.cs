f6cfb8da09f7e182b217d13c7f17987a33fcdf8c
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-26 15:33:02 +0100

    many interface and class inheritances

---
+++ Chapter7/PlayingAroundWithInterfacesInheritance/IClown.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingAroundWithInterfacesInheritance
+{
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+        void Honk();
+    }
+    interface IScaryClown : IClown
+    {
+        string ScaryThingIHave { get; }
+        void ScareLitteChildren();
+    }
+}
---
+++ Chapter7/PlayingAroundWithInterfacesInheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PlayingAroundWithInterfacesInheritance
+{
+    class FunnyFunny : IClown
+    {
+        private readonly string funnyThingIHave;
+        public string FunnyThingIHave => funnyThingIHave;
+
+
+        public void Honk()
+        {
+            Console.WriteLine($"Hi kids! I have a {funnyThingIHave}");
+        }
+        public FunnyFunny(string funnyThingIHave)
+        {
+            this.funnyThingIHave = funnyThingIHave;
+        }
+    }
+
+    class ScaryScary : FunnyFunny, IScaryClown
+    {
+        private readonly int scaryThingCount;
+        public ScaryScary(string funnyThing, int scaryThingCount) : base(funnyThing)
+        {
+            this.scaryThingCount = scaryThingCount;
+        }
+        public string ScaryThingIHave => $"{scaryThingCount} spiders";
+
+
+
+        public void ScareLitteChildren()
+        {
+            Console.WriteLine($"Boo! Gotcha! Look at my {ScaryThingIHave}!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IClown fingersTheClown = new ScaryScary("bug red nose", 14);
+            fingersTheClown.Honk();
+            if (fingersTheClown is IScaryClown iScaryClownReference)
+                iScaryClownReference.ScareLitteChildren();
+        }
+    }
+}
