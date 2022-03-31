48badf0b89f37dc1b4725a79a62e1d9b664d97fb
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-31 13:53:01 +0200

    playing around with stacks and queues and class objects

---
+++ Chapter8/collectionsQueueAndStackPlaygroudn/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collectionsQueueAndStackPlaygroudn
+{
+    enum Flapjack
+    {
+        Crispy,
+        Soggy,
+        Browned,
+        Banana,
+    }
+
+    class Lumberjack // the person
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public Lumberjack(string name)
+        {
+            this.Name = name;
+        }
+        
+        public void TakeFlapjack(Flapjack jack)
+        {
+            flapjackStack.Push(jack);
+        }
+
+        public void EatFlapjack()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            while (flapjackStack.Count > 0)
+            {
+                Console.WriteLine($"{flapjackStack.Pop().ToString().ToLower()} flapjack");
+
+            }
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Queue<Lumberjack> myQueue = new Queue<Lumberjack>();
+            string name;
+
+            Console.WriteLine("Hello World!");
+            Console.Write("What's the first name: ");
+            while ((name = Console.ReadLine()) != "") {
+                Console.Write($"Number of flapjacks: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Lumberjack lumberjack = new Lumberjack(name);
+                    for (int i = 0; i < number; i++)
+                        lumberjack.TakeFlapjack((Flapjack)random.Next(4));
+                    myQueue.Enqueue(lumberjack);
+                }
+                Console.Write("Next lumberjack's name (blank to end): ");
+
+            }
+            while (myQueue.Count > 0)
+            {
+                Lumberjack next = myQueue.Dequeue();
+                next.EatFlapjack();
+            }
+
+          
+        }
+    }
+}
